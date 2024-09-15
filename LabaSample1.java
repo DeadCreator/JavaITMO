3733f8b7c1ffae8253cc4fda0ac5bcfb4f41a3de
GeniusOfEternity <max06safin@yandex.ru>
2024-09-15 18:30:23 +0300

    First Laba update

--- LabaSample1.java
+++ LabaSample1.java
@@ -1,22 +1,22 @@
 public class LabaSample1 {
     public static void main(String[] args) {
-        short[] w = {3, 5, 7, 9, 11, 13, 15, 17, 19}; //s array initialization
+        short[] z = {3, 5, 7, 9, 11, 13, 15, 17, 19}; //s array initialization
         float[] x = new float[17]; //x array initialization
-        double[][]q = new double[9][17];
+        double[][]w = new double[9][17];
         for (int i = 0; i < 17; i++) {
             x[i] = (float)(Math.random() * 11.0 - 7.0); //filling x array with random numbers from -7.0 to 4.0
         }
         
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 17; j++) {
-                if (w[i] == 11) {
-                    q[4][j] = Math.sin(
+                if (z[i] == 11) {
+                    w[4][j] = Math.sin(
                             Math.tan(
                                     ( Math.pow((2 / (3 * (1 - x[j])) ), 2) )
                             )
                     );
-                } else if (w[i] == 3 || w[i] == 5 || w[i] == 9 || w[i] == 19) {
-                    q[i][j] = ( 4 /
+                } else if (z[i] == 3 || z[i] == 5 || z[i] == 9 || z[i] == 19) {
+                    w[i][j] = ( 4 /
                             ( 4 +
                                     ( 0.5 /
                                             (1 - Math.pow(x[j] / 2, 2))
@@ -24,7 +24,7 @@ public class LabaSample1 {
                             )
                     );
                 } else {
-                    q[i][j] = Math.pow(
+                    w[i][j] = Math.pow(
                             Math.E,
                             Math.pow(
                                     Math.pow(
@@ -34,12 +34,18 @@ public class LabaSample1 {
                             )
                     );
                 }
+            }
+        }
 
-                System.out.printf("| %.4f | \t", q[i][j]);
+        output(w);
+    }
+
+    public static void output(double[][] arr) {
+        for (double[] doubles : arr) {
+            for (double aDouble : doubles) {
+                System.out.printf("%.4f \t", aDouble);
             }
             System.out.println();
         }
-
-
     }
 }
